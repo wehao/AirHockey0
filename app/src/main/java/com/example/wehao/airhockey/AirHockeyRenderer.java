9ecb5ab24d0e0a882468dcf93f799479cfb62c18
wehao <2564193565@qq.com>
2019-02-16 21:26:46 +0800

    change orthoM to projection matrix

--- app/src/main/java/com/example/wehao/airhockey/AirHockeyRenderer.java
+++ app/src/main/java/com/example/wehao/airhockey/AirHockeyRenderer.java
@@ -29,7 +29,7 @@ import static android.opengl.Matrix.orthoM;
 
 public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 
-    private final static int POSITION_COMPONENT_COUNT = 2;
+    private final static int POSITION_COMPONENT_COUNT = 4;
     private final static int COLOR_COMPONENT_COUNT = 3;
     private final static int BYTES_PER_FLOAT = 4;
     private FloatBuffer vertexData;
@@ -48,20 +48,20 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         mContext = context;
 
         float[] tableVertices = {
-                // Order of coordinates: X, Y, R, G, B
+                // Order of coordinates: X, Y, Z, W, R, G, B
                 // Triangle Fan
-                0f, 0f, 1f, 1f, 1f,
-                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
-                0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
-                0.5f, 0.8f, 0.7f, 0.7f, 0.7f,
-                -0.5f, 0.8f, 0.7f, 0.7f, 0.7f,
-                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                0f, 0f, 0f, 1.5f, 1f, 1f, 1f,
+                -0.5f, -0.8f, 0f, 1f, 0.7f, 0.7f, 0.7f,
+                0.5f, -0.8f, 0f, 1f, 0.7f, 0.7f, 0.7f,
+                0.5f, 0.8f, 0f, 2f,0.7f, 0.7f, 0.7f,
+                -0.5f, 0.8f, 0f, 2f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.8f, 0f, 1f, 0.7f, 0.7f, 0.7f,
                 // Line 1
-                -0.5f, 0f, 1f, 0f, 0f,
-                0.5f, 0f, 1f, 0f, 0f,
+                -0.5f, 0f, 0f, 1.5f, 1f, 0f, 0f,
+                0.5f, 0f, 0f, 1.5f, 1f, 0f, 0f,
                 // Mallets
-                0f, -0.4f, 0f, 0f, 1f,
-                0f, 0.4f, 1f, 0f, 0f
+                0f, -0.4f, 0f, 1.25f, 0f, 0f, 1f,
+                0f, 0.4f, 0f, 1.75f, 1f, 0f, 0f
         };
 
         vertexData = ByteBuffer.allocateDirect(tableVertices.length * BYTES_PER_FLOAT)
